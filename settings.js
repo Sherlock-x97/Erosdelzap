853ca7522ec82853528e9abf7a8891d9d5e22730
Sherlock-x97 <105366745+Sherlock-x97@users.noreply.github.com>
2022-09-09 11:38:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@
 //i give permission to do as long as you
 //put Xeons youtube channel link in the video
 //description and tag me on githuh repo, 
-//thank you🦄
+//thank you⚡
 //════════════════════════════//
 
 const fs = require('fs')
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['919072593964'] //ur owner number
+global.ownername = "⚡Eros Del Zap" //ur owner name
+global.ytname = "YT: Eros Del Zap" //ur yt chanel name
+global.socialm = "GitHub: Sherlockx97" //ur github or insta name
+global.location = "India, Kerala, Malappuram" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['919072593964']
+global.ownertag = '919072593964' //ur tag number
+global.botname = 'Eros Del Zap ⚡' //ur bot name
+global.linkz = "https://youtu.be/ErosDelzap" //your theme url which will be displayed on whatsapp
+global.websitex = "Comming Soon" //ur website to be displayed
+global.botscript = 'https://github.com/Sherlockx97/Erosdelzap' //script link
+global.reactmoji = "⚡" //ur menu react emoji
+global.themeemoji = "⚡" //ur theme emoji
+global.packname = "ErosDelZap\n\n\n\n\n\n\n\n\n\n\n⚡⚡⚡ " //ur sticker watermark packname
+global.author = "⚡Eros Del Zap" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['919072593964'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
